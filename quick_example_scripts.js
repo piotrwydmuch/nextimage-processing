ef731c64a920dc85e4984718a157d72a50939e15
Piotr <piotrwydmuch95@gmail.com>
2021-12-09 18:44:16 +0100

    little code cleaning + html styling

--- quick_example_scripts.js
+++ quick_example_scripts.js
@@ -2,6 +2,7 @@ var Module = {
   onRuntimeInitialized: function () {
     //just sample test:
     // console.log(Module.test([120]));
+    
     function getPixelsFromImageUrl(url) {
       let canvas = document.createElement("canvas");
       let ctx = canvas.getContext("2d");
@@ -23,7 +24,6 @@ var Module = {
       let newImg = document.getElementById("newImgJS");
       let canvas = document.createElement("canvas");
       let ctx = canvas.getContext("2d");
-      let image = new Image();
       canvas.width = imageData.width;
       canvas.height = imageData.height;
       let data = [...imageData.data];
@@ -34,7 +34,6 @@ var Module = {
         data[i + 1] = avg; // green
         data[i + 2] = avg; // blue
       }
-      console.log(imageData);
       ctx.putImageData(new ImageData(
         new Uint8ClampedArray(data),
         imageData.width,
@@ -48,18 +47,13 @@ var Module = {
       const newImg = document.getElementById("newImgCPP");
       const canvas = document.createElement("canvas");
       const ctx = canvas.getContext("2d");
-      const image = new Image();
       canvas.width = imageData.width;
       canvas.height = imageData.height;
       let data = imageData.data;
-      // this result can be used for new image :)
-      // pixel manipulation should be in C++ (Module.test)
+
+      // WASM magic is here 
       newData = Module.test(data);
-      // for (var i = 0; i < newData.length; i += 4) {
-      //   data[i]     = newData[i]; // red
-      //   data[i + 1] = newData[i + 1]; // green
-      //   data[i + 2] = newData[i + 2]; // blue
-      // }
+
       ctx.putImageData(
         new ImageData(
           new Uint8ClampedArray(newData),
