a390e2873a8d74f5aef2e1cf1dcfc7b96a5659fd
Piotr <piotrwydmuch95@gmail.com>
2023-03-12 18:00:18 +0100

    some changes in comparing tool

--- backend/comparePhotos.py
+++ backend/comparePhotos.py
@@ -6,7 +6,6 @@ img_a = Image.open("a.png")
 img_b = Image.open("b.png")
 img_diff = Image.new("RGBA", img_a.size)
 
-# note how there is no need to specify dimensions
-mismatch = pixelmatch(img_a, img_b, img_diff, includeAA=True)
+mismatch = pixelmatch(img_a, img_b, img_diff, includeAA=True, threshold=0.3)
 
 img_diff.save("diff.png")
